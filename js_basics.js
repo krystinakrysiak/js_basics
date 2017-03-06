4a11664b1db73ea4183e91bfc0a4eb73327f19ed
Krystina Krysiak <krystina.krysiak@gmail.com>
2017-03-05 22:34:08 -0500

    Initial Commit.

---
+++ js_basics.js
@@ -0,0 +1,16 @@
+function door(doorNumber) {
+  if(doorNumber == 1) alert("You Win The First Prize")
+  if(doorNumber == 2) alert("You Win The Second Prize")
+  if(doorNumber == 3) alert("You Win The Third Prize")
+}
+
+
+
+function add(first, second) {
+  console.log(first + second);
+}
+
+ramones = ["Joey", "Johnny", "Dee Dee", "Marky"]
+for(var i = 0; i< ramones.length; i++){
+  console.log(ramones[i])
+}
